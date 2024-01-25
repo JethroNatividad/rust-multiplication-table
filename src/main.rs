c9ea724aca1872bbf57078ef6c93b13d1c8fcf4d
JethroNatividad <natividad.jet@gmail.com>
2024-01-26 05:46:56 +0800

    expand problem

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 // Create a program that generates multiplication tables for the numbers 0 through 12.
+// Inputs: fixed 12
+// Process: generate multiplication table from 0 - 12
+// Ouputs: show the multiplication table
 
 fn main() {
     println!("Hello, world!");
